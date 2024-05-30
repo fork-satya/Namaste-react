f43912bcc967f0d853d7c39b57432515614bee35
Acharya <satyanshu.acharya@duckcreek.com>
2024-05-30 12:31:11 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,20 @@
+//import React from 'react';
+
+const parent = React.createElement(
+    "div",
+    {id:'parent',xyz:'something'},
+    [
+        React.createElement("div",{id:"child"},[
+            React.createElement("h1",{id:"heading1"},"H1"),
+            React.createElement("h2",{id:"heading2"},"H2")
+        ]),
+        React.createElement("div",{id:"child2"},[
+            React.createElement("h1",{id:"heading3"},"H1"),
+            React.createElement("h2",{id:"heading4"},"H2")
+        ])
+    ]
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
+
